62144ee78b0f8682816bd5fd068344b39766eb53
AMRUTAM SUNIL <sunil.amrutam@nutantek.com>
2019-09-10 09:49:58 +0530

    correction1

--- public/dbconfig.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn=mysqli_connect("localhost","root","","try")or die(mysqli_error());
-?>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix'=>'user','as'=>'user.'],function(){
+    Route::get('/dashboard','UserController@dashboard')->name('dashboard');
+});
